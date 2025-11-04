336ed451051e6d3ac051f867ff1c0aeddfa85734
= <=>
2025-11-04 12:55:54 +0400

    Enhance TermiosManager to provide feedback on key presses during input

--- include/termios.cpp
+++ include/termios.cpp
@@ -35,11 +35,27 @@ public:
         {
             pushed_character = getchar();
             printf("You pressed: %c\n", pushed_character);
-            
-        }   
-    }
 
-   
+            //input read
+            switch (pushed_character)
+            {
+                case 27:
+                    printf("You pressed: Escape\n");
+                    break;
+                case 32:
+                    printf("You pressed: Space\n");
+                    break;
+                case 10:
+                    printf("You pressed: Enter\n");
+                    break;
+                case 127:
+                    printf("You pressed: Backspace\n");
+                    break;
+                default:
+                    break;
+            };   
+        }
+    };
 };
 
 int main(){
